d94da30c47fa16880714e001abed0190cd4c954a
Nicholas Elpedes <nelpedes@cynerge.com>
2023-10-02 15:41:12 -0400

    Start adding functionality

--- app.js
+++ app.js
@@ -9,12 +9,36 @@ const server = http.createServer(app);
 const io = new Server(server);
 const port = 3000;
 const __dirname = path.resolve();
+const colorContainer = {color: '#EF6262', name: 'Red'};
 
+app.use(express.static('public'));
+
+app.get('/controls', (req, res) => res.sendFile(__dirname + '/views/controls.html'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
   socket.on('disconnect', () => console.log('user disconnected'));
+
+  // join room corresponding to socket type
+  if(socket.handshake.query.type == 'control') {
+    console.log('A controller connected');
+    socket.join('control');
+  } else if(socket.handshake.query.type == 'display') {
+    console.log('A display connected');
+    socket.join('display');
+    // when a display socket connects, send it the current color
+    socket.emit('color change', colorContainer);
+  }
+
+  // when a color is sent from a control socket, send it to the display sockets
+  socket.on('color change', (color) => {
+    console.log(color);
+    colorContainer.color = color.color;
+    colorContainer.name = color.name;
+    io.to('display').emit('color change', colorContainer);
+  });
+
 });
 
-server.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.listen(port, () => console.log(`Listening on port ${port}!`));
---
+++ public/js/displayInstance.js
@@ -0,0 +1,18 @@
+// create socket connection and tell the server that this socket is for a display
+var socket = io({
+  query: {
+    type: "display",
+  },
+});
+
+// when the socket connects, log it
+socket.on("connect", () => {
+  console.log("connected");
+});
+
+// when a color is sent to a display socket, update the display with the new color and name
+socket.on("color change", (color) => {
+  console.log(color);
+  document.getElementById("display").style.backgroundColor = color.color;
+  document.getElementById("display").innerHTML = color.name;
+});
